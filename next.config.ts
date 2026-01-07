e20ac4a4424cd8defe1ec0d0231b0375f42b20a7
Sterkr <markcavellpt@gmail.com>
2026-01-07 11:37:10 +0000

    Add redirects for /wiki routes to external site
    Configured Next.js to redirect /wiki and /wiki/:path* routes to https://www.mythwarwiki.com/en, using permanent (301) redirects.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,21 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.SKIP_LINT === 'true',
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/wiki',
+        destination: 'https://www.mythwarwiki.com/en/home',
+        permanent: true, // 301
+      },
+      {
+        source: '/wiki/:path*',
+        destination: 'https://www.mythwarwiki.com/en/:path*',
+        permanent: true, // 301
+      },
+    ];
+  },
 };
 
 export default nextConfig;
